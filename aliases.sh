db4a605abaa7001aafd17b77b506d3a3d744ebad
Mark Hedley Jones <markhedleyjones@gmail.com>
2020-10-23 09:23:15 +0900

    Add aliases for sublime-text & sublime merge

--- aliases.sh
+++ aliases.sh
@@ -2,4 +2,6 @@
 # These are custom aliases
 alias arst='subl --new-window . && smerge --new-window .'
 alias cm='cd ~/catkin_ws ; catkin_make -j8; rospack profile ; source devel/setup.bash ; cd -'
+alias sm='smerge --new-window .'
+alias st='subl --new-window .'
 alias oien='cd ~/repos/sq-slammer'
