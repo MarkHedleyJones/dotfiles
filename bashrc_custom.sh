9fc8b518257e73398ada32cc114f505d6ae76bb7
Mark Hedley Jones <markhedleyjones@gmail.com>
2021-02-27 14:57:16 +0900

    Add aliases to edit NIX system configuration

--- bashrc_custom.sh
+++ bashrc_custom.sh
@@ -5,6 +5,9 @@ alias cm='cd ~/catkin_ws ; catkin_make -j8; rospack profile ; source devel/setup
 alias sm='smerge --new-window .'
 alias st='subl --new-window .'
 alias oien='cd ~/repos/sq-slammer'
+alias edit-sys='subl ~/repos/nixos-configuration/configuration.nix --new-window'
+alias dir-sys='cd ~/repos/nixos-configuration'
+alias merge-sys='sublime_merge ~/repos/nixos-configuration --new-window'
 
 xset r rate 175 25
 setxkbmap -option "shift:both_capslock"
