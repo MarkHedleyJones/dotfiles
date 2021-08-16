f0685a71635dcc69deaafef46b39dc22b22a6143
Mark Hedley Jones <markhedleyjones@gmail.com>
2021-08-16 09:49:22 +0900

    Clean up custom bash_rc file

--- bashrc_custom.sh
+++ bashrc_custom.sh
@@ -1,20 +1,26 @@
-#! /bin/bash
-# These are custom aliases
-alias arst='subl --new-window . && smerge --new-window .'
-alias cm='cd ~/catkin_ws ; catkin_make -j8; rospack profile ; source devel/setup.bash ; cd -'
-alias sm='smerge --new-window .'
-alias st='subl --new-window .'
-alias oien='cd ~/repos/sq-slammer'
-alias edit-dot='subl ~/repos/dotFiles --new-window'
-alias dir-dot='cd ~/repos/dotFiles'
-alias merge-dot='sublime_merge ~/repos/dotFiles --new-window'
-alias aliases='subl ~/repos/dotFiles/bashrc_custom.sh --new-window'
-alias cal='cal --iso --week -3'
+#!/usr/bin/env bash
+
+alias arst="subl --new-window . && smerge --new-window ."
+alias cm="cd ~/catkin_ws ; catkin_make -j$(nproc); rospack profile ; source devel/setup.bash ; cd -"
+alias sm="smerge --new-window ."
+alias st="subl --new-window ."
+alias oien="cd ~/repos/sq-slammer"
+alias edit-dot="subl ~/repos/dotFiles --new-window"
+alias dir-dot="cd ~/repos/dotFiles"
+alias merge-dot="sublime_merge ~/repos/dotFiles --new-window"
+alias aliases="subl ~/repos/dotFiles/bashrc_custom.sh --new-window"
+alias cal="cal --iso --week -3"
 
 xset r rate 175 25
 setxkbmap -option "shift:both_capslock"
 # xmodmap -e "clear Lock"
 
+LOCAL_BIN="${HOME}/.local/bin"
+
+# If a local binary folder exists but isn't in the $PATH, add it to the $PATH
+if [ -d ${LOCAL_BIN} ] && [ "$(export | grep PATH | grep ${LOCAL_BIN})" == "" ]; then
+    export PATH="${PATH}:${LOCAL_BIN}"
+fi
 
 # From https://mhoffman.github.io/2015/05/21/how-to-navigate-directories-with-the-shell.html
 alias ..="cd .."
@@ -29,5 +35,3 @@ alias ..........="cd ../../../../../../../../.."
 alias ...........="cd ../../../../../../../../../.."
 alias ............="cd ../../../../../../../../../../.."
 alias .............="cd ../../../../../../../../../../../.."
-
-export CDPATH=".:$HOME:$HOME/repos"
