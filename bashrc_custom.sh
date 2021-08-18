fcb1420c52fc66e47d7125844f0c569c0dabaccb
Mark Hedley Jones <markhedleyjones@gmail.com>
2021-08-18 14:00:33 +0900

    Revise aliases

--- bashrc_custom.sh
+++ bashrc_custom.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
+alias aliases="subl ~/repos/dotfiles/bashrc_custom.sh --new-window"
+
+# alias cal="cal --iso --week -3"
+alias cal="ncal -w3Mb"
+
+alias dir-dot="cd ~/repos/dotfiles"
+alias edit-dot="subl ~/repos/dotfiles --new-window"
+alias merge-dot="sublime_merge ~/repos/dotfiles --new-window"
+
 alias arst="subl --new-window . && smerge --new-window ."
 alias cm="cd ~/catkin_ws ; catkin_make -j$(nproc); rospack profile ; source devel/setup.bash ; cd -"
 alias sm="smerge --new-window ."
 alias st="subl --new-window ."
-alias oien="cd ~/repos/sq-slammer"
-alias edit-dot="subl ~/repos/dotFiles --new-window"
-alias dir-dot="cd ~/repos/dotFiles"
-alias merge-dot="sublime_merge ~/repos/dotFiles --new-window"
-alias aliases="subl ~/repos/dotFiles/bashrc_custom.sh --new-window"
-alias cal="cal --iso --week -3"
+
+alias c="cd ~/catkin_ws"
+alias s="cd ~/repos/seqsense"
+alias d='cd ~/repos/dotfiles'
 
 xset r rate 175 25
 setxkbmap -option "shift:both_capslock"
